ebdd745b0769794979296f5b6e1bc6a7b7a0b329
Tasha Hankewych <tash1207@bellsouth.net>
2014-03-18 10:21:26 -0400

    modified the login layout to display signup fields as well

--- android/src/co/tashawych/collector/Login.java
+++ android/src/co/tashawych/collector/Login.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
-import co.tashawych.collector.R;
 
 public class Login extends Activity {
 
@@ -31,11 +34,55 @@ public class Login extends Activity {
 	}
 
 	public void btn_signup_clicked(View v) {
-		Toast.makeText(this, "Display Sign Up fields instead", Toast.LENGTH_SHORT).show();
+		//EditText edit_username = (EditText) findViewById(R.id.edit_username);
+		//EditText edit_password = (EditText) findViewById(R.id.edit_password);
+		EditText edit_email = (EditText) findViewById(R.id.edit_email);
+		
+		//String username = edit_username.getText().toString();
+		//String password = edit_password.getText().toString();
+		String email = edit_email.getText().toString();
+		
+		Toast.makeText(this, "Email: " + email, Toast.LENGTH_SHORT).show();
+		
+		// TODO from signup, pass a first_time boolean to profile to show mini tutorial
+		Intent profile = new Intent(this, Profile.class);
+		startActivity(profile);
 	}
 
 	public void btn_browse_clicked(View v) {
 		Toast.makeText(this, "Enter Browse Collections screen", Toast.LENGTH_SHORT).show();
+	}
+	
+	public void btn_bottom_signup_clicked(View v) {
+		final LinearLayout login_layout = (LinearLayout) findViewById(R.id.layout_login);
+		final LinearLayout signup_layout = (LinearLayout) findViewById(R.id.layout_signup);
+		
+		final Button btn_login = (Button) findViewById(R.id.btn_bottom_login);
+		final Button btn_signup = (Button) findViewById(R.id.btn_bottom_signup);
+		
+		btn_login.setVisibility(View.VISIBLE);
+		btn_signup.setVisibility(View.GONE);
+
+		login_layout.setVisibility(View.GONE);
+		Animation fade_in = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_in);
+		signup_layout.startAnimation(fade_in);
+		signup_layout.setVisibility(View.VISIBLE);
+	}
+	
+	public void btn_bottom_login_clicked(View v) {
+		final LinearLayout login_layout = (LinearLayout) findViewById(R.id.layout_login);
+		final LinearLayout signup_layout = (LinearLayout) findViewById(R.id.layout_signup);
+		
+		final Button btn_login = (Button) findViewById(R.id.btn_bottom_login);
+		final Button btn_signup = (Button) findViewById(R.id.btn_bottom_signup);
+		
+		btn_login.setVisibility(View.GONE);
+		btn_signup.setVisibility(View.VISIBLE);
+
+		signup_layout.setVisibility(View.INVISIBLE);
+		Animation fade_in = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_in);
+		login_layout.startAnimation(fade_in);
+		login_layout.setVisibility(View.VISIBLE);
 	}
 
 	@Override
